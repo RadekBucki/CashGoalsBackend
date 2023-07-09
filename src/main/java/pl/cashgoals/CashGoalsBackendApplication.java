69987824c05dc3e0fde6851e82fff62a96ec92ed
Radosław Bucki <236507@edu.p.lodz.pl>
2023-07-09 21:59:04 +0200

    #1 Initialize application with start.spring.io

---
+++ src/main/java/pl/cashgoals/CashGoalsBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.cashgoals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashGoalsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashGoalsBackendApplication.class, args);
+    }
+
+}
