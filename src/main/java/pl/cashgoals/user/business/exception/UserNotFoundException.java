24b8731ce3c6435eaa8dba410bf0ea3ad8f666dd
Radosław Bucki <236507@edu.p.lodz.pl>
2024-01-01 12:31:38 +0100

    #16 Add user identification to error message

--- src/main/java/pl/cashgoals/user/business/exception/UserNotFoundException.java
+++ src/main/java/pl/cashgoals/user/business/exception/UserNotFoundException.java
@@ -2,9 +2,11 @@ package pl.cashgoals.user.business.exception;
 
 import pl.cashgoals.utils.graphql.business.exception.GraphQLNotFoundException;
 
+import java.util.Map;
+
 public class UserNotFoundException extends GraphQLNotFoundException {
     private static final String MESSAGE = "cashgoals.user.not-found";
-    public UserNotFoundException() {
-        super(MESSAGE);
+    public UserNotFoundException(String user) {
+        super(MESSAGE, Map.of("user", user));
     }
 }
